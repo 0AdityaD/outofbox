4a6da5d1fa788889cea06b9d474c6c7f80d39314
Aditya Durvasula <d4aditya@gmail.com>
2018-05-13 23:59:27 -0500

    apt-get/

---
+++ apt-get/install.sh
@@ -0,0 +1,5 @@
+# basic apps
+sudo apt-get install vim python3 pip3 pip git zsh google-chrome-stable firefox terminator texlive-full texstudio blueman spotify haskell-platform
+
+# upgrade stack
+sudo stack upgrade
