87692383c1f39a569610a05c82d07bc8d4575fbd
Aditya Durvasula <d4aditya@gmail.com>
2018-05-26 02:40:56 -0700

    fixed brightness keys

--- i3/install.sh
+++ i3/install.sh
@@ -2,6 +2,14 @@ sudo apt-get install i3
 sudo apt-get install arandr
 sudo apt-get install i3status
 sudo apt-get install i3lock
+sudo apt install help2man
+git clone https://github.com/haikarainen/light.git
+cd light
+git checkout 38c3bf
+sudo make
+sudo make install
+cd ..
+rm -rf light
 
 sudo cp config ~/.config/i3/config
 sudo cp i3status.conf ~/.config/
