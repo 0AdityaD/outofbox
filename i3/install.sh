a68098c104a20a1e4efb5b5a09087e4ad1e235c9
Aditya Durvasula <d4aditya@gmail.com>
2018-05-26 01:40:17 -0700

    more stuff

--- i3/install.sh
+++ i3/install.sh
@@ -2,3 +2,6 @@ sudo apt-get install i3
 sudo apt-get install arandr
 sudo apt-get install i3status
 sudo apt-get install i3lock
+
+sudo cp config ~/.config/i3/config
+sudo cp i3status.conf ~/.config/
